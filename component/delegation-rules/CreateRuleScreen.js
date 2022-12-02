e1d8dc826a713365063c9c67418a08d4dd7cab12
unknown <ganesh.kalje@gmail.com>
2022-12-02 20:02:58 +0530

    change for radio button

--- component/delegation-rules/CreateRuleScreen.js
+++ component/delegation-rules/CreateRuleScreen.js
@@ -1,31 +1,120 @@
-import { Text, View, Button, StyleSheet } from 'react-native';
+import { Text, View, Button, StyleSheet, TextInput, Pressable } from 'react-native';
+import RadioForm, {RadioButton, RadioButtonInput, RadioButtonLabel} from 'react-native-simple-radio-button';
+
+
+
 
 const CreateRuleScreen = () => {
+    var radio_props = [
+        {label: 'param1', value: 0 },
+        {label: 'param2', value: 1 }
+      ];
+
+    const getInitialState = () => {
+        return {
+            value: 0,
+        }
+    }
+
     return (
-        <View style={[styles.card, styles.shadowProp]} >
-            <Text>Settings!</Text>
+        <View style={styles.container}>
+            <View style={{marginBottom: 20}}>
+                <Text style={styles.label}>Approval Type:</Text>
+                <TextInput style={styles.textInput} />
+            </View>
+
+            <View style={{marginBottom: 20}}>
+                <Text style={styles.label}>*Start Date:</Text>
+                <TextInput style={styles.textInput} />
+            </View>
+
+            <View style={{marginBottom: 20}}>
+                <Text style={styles.label}>*End Date:</Text>
+                <TextInput style={styles.textInput} />
+            </View>
+
+            <View>
+                <Text style={styles.label}>Notes:</Text>
+                <TextInput multiline={true} style={styles.textArea} numberOfLines={5} />
+            </View>
+
+            <View style={{marginBottom: 20}}>
+                <Text style={styles.label}>*Reassing To:</Text>
+                <TextInput style={styles.textInput} />
+            </View>
+
+            <View style={{ marginBottom: 20 }}>
+                <RadioForm buttonSize={10}
+                    buttonOuterSize={20}
+                    radio_props={radio_props}
+                    initial={0}
+                    onPress={(value) => { }}
+                />
+            </View>
+
+            <View style={{ marginBottom: 20 }}>
+                <Pressable style={styles.buttonContainer}>
+                    <Text style={styles.buttonStlye}>Submit</Text>
+                </Pressable>
+            </View>
+           
+
+            <View style={{marginBottom: 20}}>
+                <Text style={{fontSize: 10}}>Fields marked with * are mandatory</Text>
+            </View>
+
         </View>
     );
 }
 
 const styles = StyleSheet.create({
-    card: {
-        padding: 10,
+    container: {
+        padding: 20
+    },
+    textInput: {
         backgroundColor: '#fff',
-        margin: 7.5,
-        position: 'relative',
-        borderRadius: 6,
-        backgroundPosition: 10,
-        paddingVertical: 20,
-        paddingHorizontal: 20,
-        marginVertical: 10
+        border: 1,
+        borderColor: '#2b9cd8',
+        borderStyle: 'solid',
+        borderWidth: 1,
+        padding: 0,
+        margin: 0,
+        height: 40
     },
-    shadowProp: {
-        shadowOffset: { width: -2, height: 4 },
-        shadowOpacity: 0.2,
-        shadowRadius: 3,
-        shadowColor: '#52006A',
-        elevation: 20,
+    textArea: {
+        backgroundColor: '#fff',
+        textAlignVertical: 'top',
+        border: 1,
+        borderColor: '#2b9cd8',
+        borderStyle: 'solid',
+        borderWidth: 1,
+        padding: 0,
+        margin: 0,
     },
+    label: {
+        color: '#2a2c2d',
+        fontSize: 12,
+        fontWeight: '400',
+        marginBottom: 5
+    },
+
+    buttonContainer: {
+        marginRight: 20,
+        marginLeft: 20,
+        marginTop: 10,
+        paddingTop: 10,
+        paddingBottom: 10,
+        backgroundColor: '#2b9cd8',
+        borderRadius: 25,
+        borderWidth: 1,
+        borderColor: '#fff',
+        width: '40%',
+        textAlign: 'center',
+    },
+    buttonStlye: {
+        color: '#fff',
+        fontSize: 18,
+        textAlign: 'center',
+    }
 });
 export default CreateRuleScreen;
